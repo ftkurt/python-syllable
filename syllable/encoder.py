7fb8d03ad59193dad4c2ae99edc4f783f08b061f
Fatih Kurt <ftkurt@gmail.com>
2018-01-28 08:12:37 +0300

    Required libraries updated

--- syllable/encoder.py
+++ syllable/encoder.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 """ An encoder which learns byte pair encodings for white-space separated text.  Can tokenize, encode, and decode. """
 from collections import Counter
-import string
-import pickle
 
 try:
     from typing import Dict, Iterable, Callable, List, Any, Iterator
+    import string
+    import pickle
 except ImportError:
     pass
 
