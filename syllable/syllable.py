9e259d106c64a72c176d10ce41ee6320a067d5ad
Fatih Kurt <ftkurt@gmail.com>
2018-01-31 00:38:10 +0300

    Unicode char mapping fixes

--- syllable/syllable.py
+++ syllable/syllable.py
@@ -23,21 +23,37 @@ class Encoder:
         self.lang = lang
         self.limitby = limitby
         self.limit = limit
+        self.special_chars = {}
+        self.unicode_char_mapping = {}
         if self.lang not in supported_languages:
             raise Exception('Language not supported: '+lang)
         
-        self.vovels = ["a","e","i","o","u","A","E","I","O","U"]
+        self.vowels = "aeiouAEIOU"
+        self.vowel_map = [False for i in range(256)]
+        for c in self.vowels:
+            self.vowel_map[ord(c)] = True
         self.load_package_vec()
         
-    def is_vovel(self, c):
-        return unidecode.unidecode(c) in self.vovels
+    def is_vowel(self, c):
+        if c not in self.special_chars:
+            if c in self.unicode_char_mapping:
+                return self.vowel_map[ord(self.unicode_char_mapping[c])]
+            else:
+                c_ = unidecode.unidecode(c)
+                self.unicode_char_mapping[c] = c_
+                if len(c_)==1:
+                    return self.vowel_map[ord(c_)]
+                else:
+                    self.special_chars[c] = c_
+                    return False
+        return False
     
     def char_is_special(self, c):
-        return c in string.punctuation
+        return c in string.punctuation or c in self.special_chars
     
-    def has_vovel(self, text):
+    def has_vowel(self, text):
         for c in text:
-            if is_vovel(c):
+            if self.is_vowel(c):
                 return True
         return False
     
@@ -56,12 +72,9 @@ class Encoder:
             raise Exception('Unsupported limit type: '+self.limitby)
     
     def decode_word(self, word):
-        vovel = []
-        for c in word:
-            vovel.append(self.is_vovel(c))
-
+        vowel = [self.is_vowel(c) for c in word]
         last_ind = 0
-        for i,c in enumerate(vovel):
+        for i,c in enumerate(vowel):
             if self.char_is_special(word[i]):
                 if last_ind<i:
                     yield word[last_ind:i]
@@ -71,19 +84,19 @@ class Encoder:
                 continue
             elif last_ind == i:
                 continue
-            elif c and vovel[i-1]:
+            elif c and vowel[i-1]:
                 yield word[last_ind:i]
                 last_ind = i
-            elif c and not vovel[i-1] and last_ind<i-1:
+            elif c and not vowel[i-1] and last_ind<i-1:
                 str_ = word[last_ind:i-1]
-                if self.has_vovel(str_):
+                if self.has_vowel(str_):
                     yield str_
                     last_ind = i-1
                 else:
                     continue
-            elif c and not vovel[i-1] and last_ind>=i-1:
+            elif c and not vowel[i-1] and last_ind>=i-1:
                 continue
-            elif not c and vovel[i-1]:
+            elif not c and vowel[i-1]:
                 continue
             else:
                 continue
@@ -93,7 +106,11 @@ class Encoder:
     def decode(self, text):
         words = text.split()
         for w in words:
-            yield list(self.decode_word(w))
+            try:
+                yield list(self.decode_word(w))
+            except:
+                print("Error with word: "+w)
+            
         
     def fit(self,corpus):
         self.vocab = {}
