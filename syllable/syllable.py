9531c215c944f90a128db345b41b673fae56bd5f
Fatih Kurt <ftkurt@gmail.com>
2018-10-22 22:16:50 +0200

    Update syllable.py

--- syllable/syllable.py
+++ syllable/syllable.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-""" An encoder which learns byte pair encodings for white-space separated text.  Can tokenize, encode, and decode. """
+""" An encoder which learns byte syllable classes for text.  Can tokenize, encode, and decode. """
 from collections import Counter
 
 try:
